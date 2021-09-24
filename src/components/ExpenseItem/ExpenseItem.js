c7f44893851ae35bdea14c599f8bff9d38c50dfa
Sulthan Mohaideen <sulthanmohaideen1998@gmail.com>
2021-09-24 11:12:03 +0530

    🔨 Modifed: Making structure of app using JSX

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,5 +1,15 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-  return <h3>This is visible from ExpenseItem component</h3>;
+  return (
+    <div className="expense-item">
+      <div>September 23th 2021</div>
+      <div className="expense-item__description">
+        <h2>Home Provision</h2>
+        <div className="expense-item__price">$147.65</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
