99a14c6aabb8eb5b0bf4f4b2d0812109e3922e8f
Sulthan Mohaideen <sulthanmohaideen1998@gmail.com>
2021-09-28 23:34:21 +0530

    ✏️ Fixed: Spaces to tabs & single quotes

--- src/App.js
+++ src/App.js
@@ -1,45 +1,45 @@
-import Expense from "./components/Expense/Expense";
+import Expense from './components/Expense/Expense';
 
 function App() {
-  const expenseData = [
-    {
-      id: "e001",
-      title: "Kindle eBooks",
-      price: 39.99,
-      date: new Date(2021, 1, 10),
-    },
-    {
-      id: "e002",
-      title: "Home Utensils",
-      price: 355.42,
-      date: new Date(2021, 3, 11),
-    },
-    {
-      id: "e003",
-      title: "Fuel(monthly)",
-      price: 470.61,
-      date: new Date(2021, 5, 12),
-    },
-    {
-      id: "e004",
-      title: "Desk Lamp",
-      price: 43.57,
-      date: new Date(2021, 7, 13),
-    },
-    {
-      id: "e005",
-      title: "Groceries",
-      price: 72.29,
-      date: new Date(2021, 9, 14),
-    },
-  ];
+    const expenseData = [
+        {
+            id: 'e001',
+            title: 'Kindle eBooks',
+            price: 39.99,
+            date: new Date(2021, 1, 10),
+        },
+        {
+            id: 'e002',
+            title: 'Home Utensils',
+            price: 355.42,
+            date: new Date(2021, 3, 11),
+        },
+        {
+            id: 'e003',
+            title: 'Fuel(monthly)',
+            price: 470.61,
+            date: new Date(2021, 5, 12),
+        },
+        {
+            id: 'e004',
+            title: 'Desk Lamp',
+            price: 43.57,
+            date: new Date(2021, 7, 13),
+        },
+        {
+            id: 'e005',
+            title: 'Groceries',
+            price: 72.29,
+            date: new Date(2021, 9, 14),
+        },
+    ];
 
-  return (
-    <div>
-      <h2>Expense Tracker</h2>
-      <Expense data={expenseData} />
-    </div>
-  );
+    return (
+        <div>
+            <h2>Expense Tracker</h2>
+            <Expense data={expenseData} />
+        </div>
+    );
 }
 
 export default App;
