d07b3946b6a51ded551c72f3c16c0c60aa4a4255
Sulthan Mohaideen <sulthanmohaideen1998@gmail.com>
2021-09-28 23:36:27 +0530

    🔨 Modified: Changing data via States

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,18 +1,28 @@
-import Card from "../Card/Card";
-import ExpenseDate from "../ExpenseDate/ExpenseDate";
+import { useState } from 'react';
 
-import "./ExpenseItem.css";
+import Card from '../Card/Card';
+import ExpenseDate from '../ExpenseDate/ExpenseDate';
+
+import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-  return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.price}</div>
-      </div>
-    </Card>
-  );
+    const [title, setTitle] = useState(props.title);
+
+    const titleChangeHandler = () => {
+        setTitle('New title');
+        console.log(title);
+    };
+
+    return (
+        <Card className='expense-item'>
+            <ExpenseDate date={props.date} />
+            <div className='expense-item__description'>
+                <h2>{title}</h2>
+                <div className='expense-item__price'>${props.price}</div>
+            </div>
+            <button onClick={titleChangeHandler}>Change Title</button>
+        </Card>
+    );
 }
 
 export default ExpenseItem;
