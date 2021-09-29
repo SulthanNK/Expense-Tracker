17f7b5addb4bd84793f1bd2151ef281ffa23f88b
Sulthan Mohaideen <sulthanmohaideen1998@gmail.com>
2021-09-29 15:07:52 +0530

    🔨 Modified: Using multiple state to listen inputs

--- src/components/ExpenseForm/ExpenseForm.js
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -1,20 +1,48 @@
+import { useState } from 'react';
+
 import './ExpenseForm.css';
 
 function ExpenseForm() {
+  const [enteredtitle, setEnteredTitle] = useState('');
+  const [enteredPrice, setEnteredPrice] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+
+  function titleChangeHandler(event) {
+    setEnteredTitle(event.target.value);
+  }
+
+  function priceChangeHandler(event) {
+    setEnteredPrice(event.target.value);
+  }
+
+  function dateChangeHandler(event) {
+    setEnteredDate(event.target.value);
+  }
+
   return (
     <form>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' />
+          <input type='text' onChange={titleChangeHandler} />
         </div>
         <div className='new-expense__control'>
           <label>Price</label>
-          <input type='number' min='0.01' step='0.01' />
+          <input
+            type='number'
+            min='0.01'
+            step='0.01'
+            onChange={priceChangeHandler}
+          />
         </div>
         <div className='new-expense__control'>
           <label>Date</label>
-          <input type='date' min='2020-01-01' step='2023-12-31' />
+          <input
+            type='date'
+            min='2020-01-01'
+            step='2023-12-31'
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className='new-expense__actions'>
