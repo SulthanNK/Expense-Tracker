9e2f320d4068e16164a852187e4a28fbae5df0fe
Sulthan Mohaideen <sulthanmohaideen1998@gmail.com>
2021-10-02 22:33:34 +0530

    🩹 Modified: Moved to ES6 arrow function syntax

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -5,24 +5,24 @@ import ExpenseDate from '../ExpenseDate/ExpenseDate';
 
 import './ExpenseItem.css';
 
-function ExpenseItem(props) {
-    const [title, setTitle] = useState(props.title);
+const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
 
-    const titleChangeHandler = () => {
-        setTitle('New title');
-        console.log(title);
-    };
+  const titleChangeHandler = () => {
+    setTitle('New title');
+    console.log(title);
+  };
 
-    return (
-        <Card className='expense-item'>
-            <ExpenseDate date={props.date} />
-            <div className='expense-item__description'>
-                <h2>{title}</h2>
-                <div className='expense-item__price'>${props.price}</div>
-            </div>
-            <button onClick={titleChangeHandler}>Change Title</button>
-        </Card>
-    );
-}
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>${props.price}</div>
+      </div>
+      <button onClick={titleChangeHandler}>Change Title</button>
+    </Card>
+  );
+};
 
 export default ExpenseItem;
