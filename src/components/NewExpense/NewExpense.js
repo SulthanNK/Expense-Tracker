94d5cbbf00fe66476d2f682bab102823b8a77773
Sulthan Mohaideen <imsulthannk@gmail.com>
2021-12-20 13:48:16 +0530

    Add: App title & button to display form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,8 +1,20 @@
+import { useState } from 'react';
+
 import ExpenseForm from '../ExpenseForm/ExpenseForm';
 
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+  const [formDisplay, setFormDisplay] = useState();
+
+  const formDisplayHandler = () => {
+    setFormDisplay(true);
+  };
+
+  const formHideHandler = () => {
+    setFormDisplay(false);
+  };
+
   const dataPassHandler = (savedExpenseData) => {
     const expenseData = {
       ...savedExpenseData,
@@ -10,11 +22,25 @@ const NewExpense = (props) => {
     };
 
     props.onDataPass(expenseData);
+
+    setFormDisplay(false);
   };
 
   return (
     <div className='new-expense'>
-      <ExpenseForm onDataPass={dataPassHandler} />
+      <div className='app__head'>
+        <div>
+          {!formDisplay && <h2 className='app__title'>Expense Tracker</h2>}
+        </div>
+        <div className='app__button'>
+          {!formDisplay && (
+            <button onClick={formDisplayHandler}>New Expense</button>
+          )}
+        </div>
+      </div>
+      {formDisplay && (
+        <ExpenseForm onDataPass={dataPassHandler} onCancel={formHideHandler} />
+      )}
     </div>
   );
 };
